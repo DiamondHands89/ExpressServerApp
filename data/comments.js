6104995bd57ddeae4f0d8a09616da9c858c69983
DiamondHands89 <kwamemullins@outlook.com>
2025-01-23 18:18:59 -0500

    comments data

---
+++ data/comments.js
@@ -0,0 +1,22 @@
+const comments = [
+    { 
+        id: 1, 
+        postId: 1, 
+        content: 'Great post!', 
+        userId: 2 
+    },
+    { 
+        id: 2, 
+        postId: 1, 
+        content: 'Thanks for sharing.', 
+        userId: 3 
+    },
+    { 
+        id: 3, 
+        postId: 2, 
+        content: 'Very informative.', 
+        userId: 1 
+    }
+  ];
+  
+  module.exports = comments;
