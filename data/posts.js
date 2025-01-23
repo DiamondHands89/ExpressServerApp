db24fc86c3ded697b42b628f54f03099aead106b
DiamondHands89 <kwamemullins@outlook.com>
2025-01-23 18:16:47 -0500

    posts data

---
+++ data/posts.js
@@ -0,0 +1,22 @@
+const posts = [
+    { 
+        id: 1, 
+        title: 'First Post', 
+        content: 'This is the content of the first post.', 
+        userId: 1 
+    },
+    { 
+        id: 2, 
+        title: 'Second Post', 
+        content: 'This is the content of the second post.', 
+        userId: 2 
+    },
+    { 
+        id: 3, 
+        title: 'Third Post', 
+        content: 'This is the content of the third post.', 
+        userId: 1 
+    }
+  ];
+  
+  module.exports = posts;
