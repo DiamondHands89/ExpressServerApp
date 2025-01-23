df6b4c027cb17777045db90b2084a8e6941ac598
DiamondHands89 <kwamemullins@outlook.com>
2025-01-23 17:45:44 -0500

    logging middleware

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Cusom Logging Middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+    next();
+};
+
+// Custom Authentication Middleware
+const authenticate = (req, res, next) => {
+    
+}
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Diamond Verse');
 });
